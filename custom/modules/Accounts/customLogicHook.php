b7c4ef97d020fb8f4867d98ed82aed63e55fcc71
Hasan Ghazzawi <ghazzawi95@gmail.com>
2016-04-21 16:28:25 -0400

    added comments

--- custom/modules/Accounts/customLogicHook.php
+++ custom/modules/Accounts/customLogicHook.php
@@ -1,6 +1,12 @@
 <?php
+
     class hookExample
     {
+        /*
+          A basic logic hook example
+          @author  Hasan Ghazzawi
+          @version 1.0 21/4/2016
+        */
         function hookMethod($bean, $event, $arguments)
         {
             $bean->description="this works";
